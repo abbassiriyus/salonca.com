395a468b311205bc4e77c4aec881635f0f696bd6
abbas <86982819+abbassiriyus@users.noreply.github.com>
2023-11-30 16:32:13 +0500

    sa
    sa

--- db.js
+++ db.js
@@ -2,9 +2,9 @@ require("dotenv").config()
 const { Pool } =require('pg')
 // const { Client } =require('pg')
 
-const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
-})
+// const pool = new Pool({
+//   connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+// })
 // const pool = new Client({
 //     user: "abbasuz1_abbas",
 //     host: "localhost",
@@ -12,12 +12,12 @@ const pool = new Pool({
 //     password: ")!jsaee{^2j(",
 // })
 // post 443
-// const pool = new Client({
-//     user: "u2363199_abbas",
-//     host: "localhost",
-//     database: "u2363199_salonca",
-//     password: "Tn3RDK6V5W2ifuG",
-// })
+const pool = new Client({
+    user: "u2363199_abbas",
+    host: "localhost",
+    database: "u2363199_salonca",
+    password: "Tn3RDK6V5W2ifuG",
+})
 pool.connect(err => {
     if(err) {
         console.log("Connect Error");
